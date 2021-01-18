01f63d4f17f87a7954cd0c88279cbb8c50050f1c
Yuriy Nikolaev <warlokk83@gmail.com>
2021-01-18 22:28:36 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 11555;
+        int mileCost = 20;
+        int miles = ticketPrice / mileCost;
+        System.out.println("Миль начислено: " + miles);
+    }
+}
